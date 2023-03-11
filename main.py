17c41a1247e22360b594cff14ec705392e201256
Salgado2004 <leosalgado2004@gmail.com>
2023-03-11 16:45:07 -0300

    predict cue ball trajectory

--- main.py
+++ main.py
@@ -60,7 +60,8 @@ def findTaco(img):
             x, y, w, h = cv2.boundingRect(approx)
             if objCor > 8:
                 cv2.rectangle(imgFiltered, (x, y), (x+w, y+h), (255,255,255), 2)
-                cv2.putText(imgCropped, "Taco", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6,  (0,0,0), 2)
+                cv2.putText(imgCropped, "Taco", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4,  (0,0,0), 1)
+                return [x, y, w, h]
 
 def findCueBall(img):
     croppedImg = img[51:349, 31:510]
@@ -78,7 +79,8 @@ def findCueBall(img):
             objCor = len(approx)
             x, y, w, h = cv2.boundingRect(approx)
             if objCor >= 8:
-                cv2.putText(imgCropped, "Bola branca", (x+(w//2)+40, y+(h//2)+61), cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (255,255,255), 2)
+                cv2.putText(imgCropped, "Bola branca", (x+(w//2)+40, y+(h//2)+61), cv2.FONT_HERSHEY_SIMPLEX, 0.4,  (255,255,255), 1)
+                return [x+31, y+51, w, h]
 
 def findColoredBalls(img):
     kernel = np.ones((5,5),np.uint8)
@@ -109,13 +111,56 @@ def findColoredBalls(img):
                 if objCor > 6 and objCor < 14:
                     ballCount += 1
                     foundBalls.append([x, y, w, h])
-                    cv2.putText(f_img, "Bola", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.8,  (255,255,255), 1)
+                    #cv2.putText(f_img, "Bola", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.8,  (255,255,255), 1)
         cv2.putText(f_img, f"Ball count: {ballCount}", (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.9,  (255,255,255), 2)
         if ballCount == 1:
             x, y, w, h = foundBalls[0][0], foundBalls[0][1], foundBalls[0][2], foundBalls[0][3]
-            cv2.putText(imgCropped, "Bola", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (0,0,255), 1)
+            cv2.putText(imgCropped, "Bola", (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4,  (0,0,255), 1)
+            return foundBalls[0]
     coloredBalls = stackImages(0.6, [[filteredImgs[0], filteredImgs[1], filteredImgs[2], filteredImgs[3]], [filteredImgs[4], filteredImgs[5], filteredImgs[6], filteredImgs[7]]])
-    return coloredBalls
+
+def lineEquation(point1, point2):
+    x1, y1 = point1[0], point1[1]
+    x2, y2 = point2[0], point2[1]
+    m = (y2-y1)/(x2-x1)
+    n = y1-(m*x1)
+    return m, n
+
+def dottedLine(img,pt1,pt2,color):
+    dist =((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)**.5
+    pts= []
+    for i in  np.arange(0,dist,15):
+        r=i/dist
+        x=int((pt1[0]*(1-r)+pt2[0]*r)+.5)
+        y=int((pt1[1]*(1-r)+pt2[1]*r)+.5)
+        p = (x,y)
+        pts.append(p)
+    for p in pts:
+            cv2.circle(img,p,2,color,-1)
+
+def trajectoriaPrediction(taco, cueBall, coloredBalls):
+    try:
+        #cv2.line(imgCropped, (taco[0]+taco[2]//2, taco[1]+taco[3]//2), (cueBall[0]+cueBall[2]//2, cueBall[1]+cueBall[3]//2), (255,0,0), 2)
+        
+        #Cue ball to colored ball
+        m1, n1 = lineEquation([taco[0]+taco[2]//2, taco[1]+taco[3]//2], [cueBall[0]+cueBall[2]//2, cueBall[1]+cueBall[3]//2])
+        x1 = (coloredBalls[0]+coloredBalls[2]//2)+1
+        y1 = int((m1*x1) + n1)
+        dottedLine(imgCropped, (cueBall[0]+cueBall[2]//2, cueBall[1]+cueBall[3]//2), (x1, y1), (0,200,0))
+        cv2.circle(imgCropped, (x1, y1), 5, (0,255,0), cv2.FILLED)
+
+        #Colored ball to hole
+        m2, n2 = lineEquation([x1, y1], [coloredBalls[0]+coloredBalls[2]//2, coloredBalls[1]+coloredBalls[3]//2])
+        xList = [pt for pt in range(30,515,5)]
+        yList = []
+        for x in xList:
+            yList.append(int((m2*x) + n2))
+        for (x, y) in zip(xList, yList):
+            cv2.circle(imgCropped, (x, y), 2, (0,0,255), cv2.FILLED)
+
+    except TypeError:
+        pass
+
 
 frameWidth = 640
 frameHeight = 480
@@ -125,10 +170,11 @@ while True:
     imgRaw = cv2.resize(frame, (frameWidth, frameHeight))
     imgCropped = imgRaw[10:400,50:591]
     cv2.imwrite("imgColors.png", imgCropped)
-    findTaco(imgCropped)
-    findCueBall(imgCropped)
+    taco = findTaco(imgCropped)
+    cueBall = findCueBall(imgCropped)
     coloredBalls = findColoredBalls(imgCropped)
+    trajectoriaPrediction(taco, cueBall, coloredBalls)
     #finalImg = stackImages(1, [imgRaw, coloredBalls])
     cv2.imshow("Result", imgRaw)
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
