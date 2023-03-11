382b32ed57cda1e3f42c60d646f3701db6eaf070
Salgado2004 <leosalgado2004@gmail.com>
2023-03-11 10:19:51 -0300

    refactoring

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ def stackImages(scale,imgArray):
         hor= np.hstack(imgArray)
         ver = hor
     return ver
+
+def colorFilter(img, lower, upper):
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgFiltered = cv2.bitwise_and(img,img,mask=mask)
+    return imgFiltered
         
 def imgProcessing(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -39,11 +45,9 @@ def imgProcessing(img):
     return imgCanny
 
 def findTaco(img):
-    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     lower = np.array([82,17,182])
     upper = np.array([99,88,255])
-    mask = cv2.inRange(imgHSV,lower,upper)
-    imgFiltered = cv2.bitwise_and(img,img,mask=mask)
+    imgFiltered = colorFilter(img, lower, upper)
     imgProcessed = imgProcessing(imgFiltered)
     contours, hierarchy = cv2.findContours(imgProcessed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for i in contours:
@@ -60,11 +64,9 @@ def findTaco(img):
 
 def findCueBall(img):
     croppedImg = img[51:349, 31:510]
-    imgHSV = cv2.cvtColor(croppedImg,cv2.COLOR_BGR2HSV)
     lower = np.array([40,9,107])
     upper = np.array([72,96,210])
-    mask = cv2.inRange(imgHSV,lower,upper)
-    imgFiltered = cv2.bitwise_and(croppedImg,croppedImg,mask=mask)
+    imgFiltered = colorFilter(croppedImg, lower, upper)
     imgProcessed = imgProcessing(imgFiltered)
     contours, hierarchy = cv2.findContours(imgProcessed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for i in contours:
@@ -78,6 +80,12 @@ def findCueBall(img):
             if objCor >= 8:
                 cv2.putText(imgCropped, "Bola branca", (x+(w//2)+40, y+(h//2)+61), cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (255,255,255), 2)
 
+def findColoredBalls(img):
+    lower = np.array([13,133,132])
+    upper = np.array([66,255,255])
+    imgFiltered = colorFilter(img, lower, upper)
+    return imgFiltered
+
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture("resources/shots.mp4")
@@ -88,7 +96,8 @@ while True:
     cv2.imwrite("imgColors.png", imgCropped)
     findTaco(imgCropped)
     findCueBall(imgCropped)
-    #finalImg = stackImages(1, [imgRaw, cueBall])
-    cv2.imshow("Result", imgRaw)
-    if cv2.waitKey(3) & 0xFF == ord('q'):
+    coloredBall = findColoredBalls(imgCropped)
+    finalImg = stackImages(1, [imgRaw, coloredBall])
+    cv2.imshow("Result", finalImg)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
